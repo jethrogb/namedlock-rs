ecd876ad9d9077f61331de21ac01db04636c1ad0
Jethro Beekman <jethro@jbeekman.nl>
2015-11-10 18:08:56 -0800

    Updated documentation to 0.4.0

--- doc/implementors/core/cmp/trait.Eq.js
+++ doc/implementors/core/cmp/trait.Eq.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['namedlock'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='enum' href='namedlock/enum.Cleanup.html' title='namedlock::Cleanup'>Cleanup</a>",];
+implementors['namedlock'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='enum' href='namedlock/enum.Cleanup.html' title='namedlock::Cleanup'>Cleanup</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
---
+++ doc/implementors/core/convert/trait.From.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['namedlock'] = ["impl&lt;T&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;<a class='struct' href='https://doc.rust-lang.org/nightly/std/sys_common/poison/struct.PoisonError.html' title='std::sys_common::poison::PoisonError'>PoisonError</a>&lt;T&gt;&gt; for <a class='struct' href='namedlock/lockresult/struct.PoisonError.html' title='namedlock::lockresult::PoisonError'>PoisonError</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
